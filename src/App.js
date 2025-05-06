d4d9773ef62890b4eeb5a7c5507323da35640c3a
SeaAdam <adameroearl@gmail.com>
2025-05-06 22:31:00 +0800

    my react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import ContactCard from "./ContactCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const contacts = [
+    {
+      avatarUrl:"https://placehold.co/150",
+      name : "Jenny Han",
+      email : "jenny.han@notreal.com",
+      age: 25
+    },
+    {
+      avatarUrl:"https://placehold.co/150",
+      name : "Paul Jones",
+      email : "paul.jones@notreal.com",
+      age: 27
+    },
+    {
+      avatarUrl:"https://placehold.co/150",
+      name : "Earll Jones",
+      email : "earll.jones@notreal.com",
+      age: 100
+    },
+    {
+      avatarUrl:"https://placehold.co/150",
+      name : "Angel Jones",
+      email : "Angel.jones@notreal.com",
+      age: 23
+    }
+ 
+  ];
+
+
+  return(
+    <div>
+      {contacts.map((contact, index)=>{
+        return(
+          <ContactCard key={index}
+          avatarUrl = {contact.avatarUrl}
+          name = {contact.name}
+          email = {contact.email}
+          age = {contact.age}
+          />
+        )
+      })}
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App; 
